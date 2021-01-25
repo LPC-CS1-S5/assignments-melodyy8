a0d591efdbb5f5b6f00bfd44ee9682ed73a7b6cb
yimelsparky <johnyi@berkeley.edu>
2021-01-25 18:29:16 +0000

    finished assignment2-2

---
+++ assignment2-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char letter;
+  letter = 65;
+  cout << letter << "\n";
+  letter = 66;
+  cout << letter << "\n";
+  return 0;
+
+  //character values are stored using the ASCII codes, which are integers, so char and int are related because of this.
+}
