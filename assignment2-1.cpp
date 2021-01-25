7129b24f1f9f127b514a76fd1a30589b826f610e
yimelsparky <johnyi@berkeley.edu>
2021-01-25 18:22:07 +0000

    finished assignment2-1

---
+++ assignment2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int checking;
+  unsigned int miles;
+  long days;
+
+  checking = -20;
+  miles = 4276;
+  days = 189000;
+
+  cout << "We have made a long journey of " << miles << " miles.\n";
+  cout << "Our checking account balance is " << checking << ".\n";
+  cout << "About " << days << " days ago Columbus stood on this spot.";
+  return 0;
+}
