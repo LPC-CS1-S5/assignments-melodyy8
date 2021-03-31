d011cab257a3cfd529afe77945a26dde065c2353
yimelsparky <johnyi@berkeley.edu>
2021-03-31 15:41:32 +0000

    worked on question 2

--- Midterm/question2.cpp
+++ Midterm/question2.cpp
@@ -12,5 +12,20 @@ int main()
     exit(0);
   }
 
-  while ()
+  string stuName;
+  int score1, score2;
+  int sum;
+  double avg;
+  int counter = 0;
+  while (ifs >> stuName >> score1 >> score2)
+  {
+    sum = score1 + score2;
+    avg = sum / 2.0;
+    if (avg > 80)
+    {
+      counter += 1;
+    }
+    cout << "Student name: " << stuName << " Score 1: " << score1 << " Score 2: " << score2 << " Sum: " << sum << " Average: " << avg << endl; 
+  }
+  cout << "The total number of students who has the average > 80: " << counter << " students\n";
 }
