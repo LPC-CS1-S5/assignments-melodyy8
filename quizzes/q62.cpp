08120159c956dde2741f9aa49414bca4c5a6830a
yimelsparky <>
2021-04-14 06:33:12 +0000

    working on problem 2 :)

---
+++ quizzes/q62.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+void gerRandnum(int &n1, int &n2, int &n3);
+// In this function, generate three random numbers and assign the values to the parameter n1, n2, and n3
+
+int  findMin(int n1, int n2, int n3); 
+// The return value : the min among n1, n2 and n3.
+
+void printResult(int n1, int n2, int n3, int min);
+// Print out the results with the original numbers and the minimum value
+
+int main()
+{
+
+}
+
+void gerRandnum(int &n1, int &n2, int &n3)
+{
+
+}
+
+int findMin(int n1, int n2, int n3)
+{
+
+}
+
+void printResult(int n1, int n2, int n3, int min)
+{
+  
+}
