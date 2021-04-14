9d58e44e96b9f8f5f9104e8208f2480d5835713d
yimelsparky <>
2021-04-14 06:46:17 +0000

    finished problem 2! :)

--- quizzes/q62.cpp
+++ quizzes/q62.cpp
@@ -14,20 +14,39 @@ void printResult(int n1, int n2, int n3, int min);
 
 int main()
 {
-
+  int n1, n2, n3;
+  gerRandnum(n1, n2, n3);
+  int min;
+  min = findMin(n1, n2, n3);
+  printResult(n1, n2, n3, min);
+  return 0;
 }
 
 void gerRandnum(int &n1, int &n2, int &n3)
 {
-
+  unsigned seed = time(0);
+  srand(seed);
+  n1 = rand();
+  n2 = rand();
+  n3 = rand();
 }
 
 int findMin(int n1, int n2, int n3)
 {
-
+  if (n1 < n2)
+  {
+    if (n1 < n3)
+      return n1;
+    return n3;
+  }
+  else if (n3 < n2)
+    return n3;
+  else
+    return n2;
 }
 
 void printResult(int n1, int n2, int n3, int min)
 {
-  
+  cout << "The three integers are: " << n1 << ", " << n2 << ", and " << n3 << endl;
+  cout << "The minimum is: " << min << endl;
 }
