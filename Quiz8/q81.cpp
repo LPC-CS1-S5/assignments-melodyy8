a54c6f36bdf11949f699ded70c2eef81952a6f47
yimelsparky <>
2021-05-12 06:06:55 +0000

    finished problem 1!

--- Quiz8/q81.cpp
+++ Quiz8/q81.cpp
@@ -9,15 +9,30 @@ int main()
   ifstream ifs;
   string s;
   string text;
+  int sum = 0;
+
+  ifs.open("python.txt");
+  if (!ifs)
+  {
+    cout << "Open Error\n";
+    exit(0);
+  }
 
   while (!ifs.eof())
   {
     ifs >> s;
-    text = text + s;
-    cout << text << endl;
+    for (int i = 0; i < s.length() - 1; i++)
+    {
+      if (s[i] == 't' || s[i] == 'T')
+      {
+        if (s[i + 1] == 'h')
+        {
+          sum += 1;
+        }
+      }
+    }
   }
-  for(int i = 0; i < text.length(); i++)
-  {
-    continue;
-  }
+
+  cout << "There are " << sum << " instances of 'th' in the text file." << endl;
+  return 0;
 }
