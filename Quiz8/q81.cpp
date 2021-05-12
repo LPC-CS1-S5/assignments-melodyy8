ab57b550c94c6a35d9a5e3d2ff816c046f7477c8
yimelsparky <>
2021-05-12 05:53:44 +0000

    working on problem 1!

--- Quiz8/q81.cpp
+++ Quiz8/q81.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  ifstream ifs;
+  string s;
+  string text;
+
+  while (!ifs.eof())
+  {
+    ifs >> s;
+    text = text + s;
+    cout << text << endl;
+  }
+  for(int i = 0; i < text.length(); i++)
+  {
+    continue;
+  }
+}
