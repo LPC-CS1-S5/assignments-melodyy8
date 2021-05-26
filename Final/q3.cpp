074f659acef77a47e61ac3f37e5c5b36a85da53c
yimelsparky <>
2021-05-26 07:10:21 +0000

    started working on q3

---
+++ Final/q3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  const int SIZE1=5;
+  const int SIZE2=10;
+  int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+  int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+  int i = 0, j = 0;
+
+  while(i < SIZE1 && j < SIZE2)
+  {
+    
+  }
+
+}
+
