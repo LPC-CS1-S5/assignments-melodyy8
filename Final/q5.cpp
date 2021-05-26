7c79446c315f9b9c693f6549270206ad60289efb
yimelsparky <>
2021-05-26 08:12:57 +0000

    a little more work on q5 :)

--- Final/q5.cpp
+++ Final/q5.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
+
+const unsigned short N=5;
+struct Scores
+{     
+  double sc[N];     
+  string grade;
+};
+
+struct Grade
+{    
+  string first;    
+  string last;   
+  string ssn;   
+  Scores score;
+};
+
+
+
+int main()
+{
+  //reading from txt file
+  ifstream ifs;
+  Grade g;
+
+  ifs.open("grades.txt");
+  
+
+  //writing into bin file
+
+}
