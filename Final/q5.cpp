f2fad5d92cc9f28e9039bd774e74819295d9fb83
yimelsparky <>
2021-05-26 08:32:29 +0000

    working on q5

--- Final/q5.cpp
+++ Final/q5.cpp
@@ -24,10 +24,26 @@ int main()
   //reading from txt file
   ifstream ifs;
   Grade g;
-
+  string line;
   ifs.open("grades.txt");
-  
+  int i = 0;
+  while (i < 16)
+  {
+    std::getline(ifs, g.last, ',');
+    std::getline(ifs, g.first, ',');
+    std::getline(ifs, g.ssn, ',');
+    string score;
+    for (int j = 0; j < N; j++)
+    {
+      std::getline(ifs, score, ',');
+      g.score.sc[j] = stoi(score);
+    }
+    std::getline(ifs, g.score.grade, ',');
+    
+    //writing into bin file
+    
+  }
 
-  //writing into bin file
+ 
 
 }
